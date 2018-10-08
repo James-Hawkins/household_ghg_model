28dc61b54984206c8c7de274e373ba36bc81a082
James Hawkins <J.Hawkins@cgiar.org>
2018-10-08 07:17:25 +0100

    _

---
+++ dairyclimatemodel2/225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmssn_nt.cmd "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\225a\" "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\" %1 "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\225a\gamscntr.dat" "C:\Program Files (x86)\GAMS23.5\"
