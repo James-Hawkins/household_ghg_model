58d803692a33f9971fcf990443b1995d14a3bb18
James Hawkins <J.Hawkins@cgiar.org>
2018-10-16 08:14:14 +0100

    updated model with new costs, results

---
+++ dairyclimatemodel/225b/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsmi_nt.cmd "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\225b\" "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\" %1 "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\225b\gamscntr.dat" "C:\Program Files (x86)\GAMS23.5\"
---
+++ dairyclimatemodel/225e/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsxitNT.cmd "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\225e\" "C:\Users\Gaming\Documents\Github\household_ghg_model\dairyclimatemodel2\" 8 8
